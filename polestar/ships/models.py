83b54646195a7e64622209e76fe38683b878935a
Javier <fjbordalas@gmail.com>
2019-05-10 01:45:01 +0200

    Move the many to many field into the Ship model

--- polestar/ships/models.py
+++ polestar/ships/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 
-class Ship(models.Model):
-    name = models.CharField(max_length=70)
-    imo_number = models.PositiveIntegerField(verbose_name="IMO Number")
-
-    def __str__(self):
-        return '{} ({})'.format(self.name, self.imo_number)
-
-    class Meta:
-        unique_together = ['name', 'imo_number']
-
-
 class Position(models.Model):
-    imo_number = models.ManyToManyField(Ship)
     date = models.DateTimeField()
     latitude = models.FloatField()
     longitude = models.FloatField()
 
     class Meta:
         ordering = ('date',)
+
+
+class Ship(models.Model):
+    name = models.CharField(max_length=70)
+    imo_number = models.PositiveIntegerField(verbose_name="IMO Number")
+    positions = models.ManyToManyField(Position)
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.imo_number)
+
+    class Meta:
+        unique_together = ['name', 'imo_number']
