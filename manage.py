e6620ff9ba6350afaa57d95eabce190a8cd6b804
Javier <fjbordalas@gmail.com>
2019-05-09 19:57:07 +0200

    Split settings according the environment

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polestar.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polestar.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
