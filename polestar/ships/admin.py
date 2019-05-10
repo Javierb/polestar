52a46e1fbb118f84bfed2fcec275b5a9d5bfc7ec
Javier <fjbordalas@gmail.com>
2019-05-10 11:28:41 +0200

    Add Positions to the admin section for testing purposes

--- polestar/ships/admin.py
+++ polestar/ships/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Ship
+from .models import Ship, Position
 
 @admin.register(Ship)
+class ShipAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Position)
 class ShipAdmin(admin.ModelAdmin):
     pass
