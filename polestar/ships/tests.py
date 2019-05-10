f7c1df48255ad2a2dba94a5687e61345cd7a9281
Javier <fjbordalas@gmail.com>
2019-05-10 13:59:28 +0200

    Loading fixtures in Ship tests

--- polestar/ships/tests.py
+++ polestar/ships/tests.py
@@ -1,45 +1,38 @@
 from django.test import TestCase
-from .models import Ship, Position
+from django.core.management.base import CommandError
 from django.db.utils import IntegrityError
 from django.utils import timezone
-
 from django.core import management
-from django.core.management.base import CommandError
+from .models import Ship, Position
 import io
 
 
 class BaseShipTestCase(TestCase):
-    def setUp(self):
-        Ship.objects.create(name="ship1", imo_number="9632179")
-        Ship.objects.create(name="ship2", imo_number="9247455")
-        Ship.objects.create(name="ship3", imo_number="9595321")
-
+    fixtures = ['ships.json',]
+    
 
 class ShipTestCase(BaseShipTestCase):
     def test_ship_str(self):
-        s1 = Ship.objects.get(name="ship1")
-        s2 = Ship.objects.get(name="ship2")
-        self.assertEqual(str(s1), 'ship1 (9632179)')
-        self.assertEqual(str(s2), 'ship2 (9247455)')
+        s1 = Ship.objects.get(imo_number="9632179")
+        self.assertEqual(str(s1), 'Mathilde Maersk (9632179)')
 
     def test_ship_integrity(self):
-        s1 = Ship.objects.get(name="ship1")
+        s1 = Ship.objects.get(imo_number="9632179")
         s3 = Ship(name=s1.name, imo_number=s1.imo_number)
         with self.assertRaises(IntegrityError):
             s3.save()
 
     def test_created_updated(self):
-        s1 = Ship.objects.get(name="ship1")
-        s2 = Ship.objects.get(name="ship2")
+        s1 = Ship.objects.get(imo_number="9632179")
+        s2 = Ship.objects.get(imo_number="9247455")
         self.assertNotEqual(s1.updated, s1.created)
-        self.assertTrue(s1.updated < s2.updated)
         s1.save()
         self.assertTrue(s1.updated > s2.updated)
 
 
 class PositionTestCase(BaseShipTestCase):
     def test_position_str(self):
-        s1 = Ship.objects.get(name="ship1")
+        s1 = Ship.objects.get(imo_number="9632179")
         s1.positions.create(date=timezone.now(), latitude=17.88356590271, longitude=-63.2951011657715)
         p1 = Position.objects.first()
         self.assertIn(p1, s1.positions.all())
