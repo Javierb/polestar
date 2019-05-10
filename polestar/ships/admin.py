c92840b8d0564fdcec1b9230dc66c1b156f6ab35
Javier <fjbordalas@gmail.com>
2019-05-10 12:20:30 +0200

    Verbose ship admin list fields

--- polestar/ships/admin.py
+++ polestar/ships/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Ship, Position
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 @admin.register(Ship)
 class ShipAdmin(admin.ModelAdmin):
-    pass
+    fields = ('name', 'imo_number')
+    list_display = ('ship','created', 'updated',)
+
+    def ship(self, obj):
+        return str(obj)
 
 @admin.register(Position)
 class ShipAdmin(admin.ModelAdmin):
