5d8dfc519ea1ccb1f34c0c9dbc2aafab6ee40167
Javier <fjbordalas@gmail.com>
2019-05-10 16:53:01 +0200

    Move DB commands to entrypoint on docker compose when the db is up

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@
 echo "Applying database migrations"
 python manage.py migrate
 
+echo "Load ships data"
+python manage.py loaddata ships
+
+echo "Import data"
+python manage.py import_data
+
 echo "Starting django dev server"
 python manage.py runserver 0.0.0.0:8000
