eeaccf4c3e18f3c2df04488f3194a0b4e1885cb0
Javier <fjbordalas@gmail.com>
2019-05-10 11:44:45 +0200

    Add api urls

--- polestar/urls.py
+++ polestar/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from polestar.api import views
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+router = routers.DefaultRouter()
+router.register(r'ships', views.ShipViewSet)
+router.register(r'positions/(?P<imo>\d+)', views.PositionList, basename='position')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
